dda3dad61c34b19ce64b59c08d238a18d1a44790
BnnQ <1v25fa94@gmail.com>
2023-03-15 14:01:12 +0200

    Minor fixes

--- Utils/Extensions/SeedExtensions.cs
+++ Utils/Extensions/SeedExtensions.cs
@@ -2,17 +2,17 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
-namespace Homework.Utils
+namespace Homework.Utils.Extensions
 {
-    public static partial class SeedExtensions
+    public static class SeedExtensions
     {
-        private readonly static string UserId = "b74ddd14-6340-4840-95c2-db12554843e5";
-        private readonly static string AdminRoleId = "fab4fac1-c546-41de-aebc-a14da6895711";
-        private readonly static string CustomerRoleId = "c7b013f0-5201-4317-abd8-c211f91b7330";
+        private const string UserId = "b74ddd14-6340-4840-95c2-db12554843e5";
+        private const string AdminRoleId = "fab4fac1-c546-41de-aebc-a14da6895711";
+        private const string CustomerRoleId = "c7b013f0-5201-4317-abd8-c211f91b7330";
 
         public static ModelBuilder SeedUsers(this ModelBuilder builder)
         {
-            User user = new User
+            var user = new User
             {
                 Id = UserId,
                 UserName = "admin",
@@ -33,8 +33,8 @@ namespace Homework.Utils
         public static ModelBuilder SeedRoles(this ModelBuilder builder)
         {
             builder.Entity<IdentityRole>().HasData(
-                new IdentityRole() { Id = AdminRoleId, Name = "Admin", ConcurrencyStamp = "1", NormalizedName = "ADMIN" },
-                new IdentityRole() { Id = CustomerRoleId, Name = "Customer", ConcurrencyStamp = "2", NormalizedName = "CUSTOMER" }
+                new IdentityRole { Id = AdminRoleId, Name = "Admin", ConcurrencyStamp = "1", NormalizedName = "ADMIN" },
+                new IdentityRole { Id = CustomerRoleId, Name = "Customer", ConcurrencyStamp = "2", NormalizedName = "CUSTOMER" }
                 );
 
             return builder;
@@ -43,7 +43,7 @@ namespace Homework.Utils
         public static ModelBuilder SeedUserRoles(this ModelBuilder builder)
         {
             builder.Entity<IdentityUserRole<string>>().HasData(
-                new IdentityUserRole<string>() { RoleId = AdminRoleId, UserId = UserId }
+                new IdentityUserRole<string> { RoleId = AdminRoleId, UserId = UserId }
                 );
 
             return builder;
